21114eae5b76faef687b07a25e80966d7db9f555
Szymon Bukowski <szymekbukowski05@gmail.com>
2026-02-05 14:07:16 +0100

    Changing agents logic, to take a player as an argument for function responsible for choosing a move. Deleted player attributes

--- connect4/agents/MoveAgent.py
+++ connect4/agents/MoveAgent.py
@@ -1,35 +1,26 @@
+from __future__ import annotations
 from abc import abstractmethod,ABC
 from typing import List, TYPE_CHECKING
 
 from connect4.marker import Marker
+
+
 if TYPE_CHECKING:
     from connect4.board import Board
+    from connect4.player import Player
 
 
 
 class MoveAgent(ABC):
-    def __init__(self,board: 'Board'):
+    def __init__(self,board: Board):
         self._valid_moves = None
-        self.__board = board
+        self._board = board
 
     def _get_possible_moves(self) -> List[int]:
-        columns = self.__board.get_numer_of_columns()
-        return [i for i in range(0,columns) if self.__board.board[0][i] == Marker.EMPTY]
+        columns = self._board.numer_of_columns
+        return [column for column in range(0, columns) if not self._board.is_column_full(column)]
 
 
     @abstractmethod
-    def choose_move(self) -> int(0-6):
-        """
-        Abstract method that enforces the implementation of a specific method
-        to choose a move. This method is designed to be overridden by
-        subclasses, mandating them to define their logic for selecting a move
-        within a specified range.
-
-        :raises NotImplementedError: If the method is not implemented in the
-            subclass that inherits this abstract method.
-        :returns: An integer representing the chosen move, constrained to the
-            range of 1 to 7.
-        :rtype: int
-        """
-
+    def choose_move(self,player: Player) -> int:
         pass
--- connect4/agents/RandomAgent.py
+++ connect4/agents/RandomAgent.py
@@ -6,6 +6,6 @@ from connect4.agents.MoveAgent import MoveAgent
 class RandomAgent(MoveAgent):
     """Class that implements a random agent"""
 
-    def choose_move(self):
+    def choose_move(self, player = None):
         return random.choice(self._get_possible_moves())
 
