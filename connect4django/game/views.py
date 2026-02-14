f8abf8e7da7984cd580c69906f15f7287de50545
Szymon Bukowski <szymekbukowski05@gmail.com>
2026-02-14 18:42:09 +0100

    Added board representation.

--- connect4django/game/views.py
+++ connect4django/game/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from connect4.board import Board
+from connect4.marker import Marker
+
+
 # Create your views here.
 def home(request):
     return render(request, 'game/home.html')
@@ -12,4 +16,10 @@ def leaderboard(request):
     return render(request, 'game/leaderboard.html')
 
 def play(request):
-    return render(request, 'game/play.html')
+    board = Board()
+    board.move(3,Marker.FIRST_PLAYER)
+    board.move(4,Marker.SECOND_PLAYER)
+    context = {
+        'board' : board.board
+    }
+    return render(request, 'game/play.html',context)
